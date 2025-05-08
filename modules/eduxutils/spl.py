397287751f682164d508c41d5b66b6164ef2004f
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-05-08 07:00:49 +0600

    SmartDev's Spell Check API

--- modules/eduxutils/spl.py
+++ modules/eduxutils/spl.py
@@ -1,31 +1,54 @@
 # Copyright @ISmartDevs
 # Channel t.me/TheSmartDev
+import aiohttp
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.enums import ParseMode
 from pyrogram.handlers import MessageHandler
-from spellchecker import SpellChecker
 from config import COMMAND_PREFIX
 
-# Initialize the spell checker
-spell = SpellChecker()
-
 async def check_spelling(word):
-    misspelled = spell.unknown([word])
-    if misspelled:
-        corrected_word = spell.correction(list(misspelled)[0])
-    else:
-        corrected_word = word
-    return corrected_word
+    url = f"https://abirthetech.serv00.net/spl.php?prompt={word}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            result = await response.json()
+            return result['response'].strip()
 
 async def spell_check(client: Client, message: Message):
-    user_input = message.text.split(maxsplit=1)
-    if len(user_input) < 2:
-        await client.send_message(message.chat.id, "**❌Provide some text to check spelling..**", parse_mode=ParseMode.MARKDOWN)
+    # Check if the message is a reply
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text.strip()
+        # Ensure reply contains a single word
+        if len(user_input.split()) != 1:
+            await client.send_message(
+                message.chat.id,
+                "**❌ Reply to a message with a single word to check spelling.**",
+                parse_mode=ParseMode.MARKDOWN
+            )
+            return
     else:
-        checking_message = await client.send_message(message.chat.id, "**Checking Spelling Please Wait...✨**", parse_mode=ParseMode.MARKDOWN)
-        corrected_word = await check_spelling(user_input[1])
-        await checking_message.edit(text=f"`{corrected_word}`", parse_mode=ParseMode.MARKDOWN)
+        # Check if command has a single word
+        user_input = message.text.split(maxsplit=1)
+        if len(user_input) < 2 or len(user_input[1].split()) != 1:
+            await client.send_message(
+                message.chat.id,
+                "**❌ Provide a single word to check spelling.**",
+                parse_mode=ParseMode.MARKDOWN
+            )
+            return
+        user_input = user_input[1].strip()
+
+    # Proceed with spell check
+    checking_message = await client.send_message(
+        message.chat.id,
+        "**Checking Spelling Please Wait...✨**",
+        parse_mode=ParseMode.MARKDOWN
+    )
+    corrected_word = await check_spelling(user_input)
+    await checking_message.edit(
+        text=f"`{corrected_word}`",
+        parse_mode=ParseMode.MARKDOWN
+    )
 
 def setup_spl_handler(app: Client):
     app.add_handler(
