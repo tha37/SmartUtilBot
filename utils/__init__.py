cb4d5512750e1c7b6270d4bd5f130e65cb65a584
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-05-25 12:12:07 +0600

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,6 +8,5 @@ from .locale_data import get_locale_for_country
 from .dc_locations import get_dc_locations
 from .payment import handle_donate_callback, DONATION_OPTIONS_TEXT, get_donation_buttons, generate_invoice, timeof_fmt
 from .genbtn import responses, main_menu_keyboard, second_menu_keyboard, third_menu_keyboard
-from .settings import setup_settings_handler
 from .pgbar import progress_bar
 from .nfy import notify_admin
