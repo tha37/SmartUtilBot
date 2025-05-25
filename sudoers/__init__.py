554ee145afd2f3db71ef03d10027989a1702719b
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-05-25 12:13:44 +0600

    Update __init__.py

--- sudoers/__init__.py
+++ sudoers/__init__.py
@@ -4,10 +4,12 @@
 from .admin.admin import setup_admin_handler
 from .logs.logs import setup_logs_handler
 from .restart.restart import setup_restart_handler
+from .sudo.sudo import setup_sudo_handler
 from .speedtest.speedtest import setup_speed_handler
 
 def setup_sudoers_handlers(app):
     setup_admin_handler(app)
     setup_logs_handler(app)
     setup_restart_handler(app)
+    setup_sudo_handler(app)
     setup_speed_handler(app)
