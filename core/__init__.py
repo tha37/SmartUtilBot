ec8bc3ae47e8d0529f87c49993fe7651e022a2b5
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-05-27 18:47:31 +0600

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,6 @@
 #Copyright @ISmartDevs
 #Channel t.me/TheSmartDev
 from .mongo import user_activity_collection
-from .database import group_settings, auth_admins
+from .database import group_settings, auth_admins, banned_users
 from .db import group_channel_bindings
 from .start import setup_start_handler
